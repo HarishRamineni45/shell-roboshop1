c0e887f2ac30f81f1815d04e8b900fa4b6d1c560
Harish <hramine@bgsu.edu>
2026-02-09 18:01:47 -0500

    Hi

--- mongodb.sh
+++ mongodb.sh
@@ -26,20 +26,20 @@ VALIDATE(){
 
 }
 
-cp mongo.repo /etc/yum/repos.d/mongo.repo
-VALIDATE $? "copying Mongo repo"
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo Repo" 
 
-dnf install mongodb-org -y 
-VALIDATE $? "Installing Mongodb"
+dnf install mongodb-org -y &>>$LOGS_FILE
+VALIDATE $? "Installing MongoDB server"
 
-systemctl enable mongod 
-VALIDATE $? "Ebanling Mongodb server"
+systemctl enable mongod &>>$LOGS_FILE
+VALIDATE $? "Enable MongoDB"
 
-systemctl start mongod 
-VALIDATE $? "Starting Mongodb server"
+systemctl start mongod
+VALIDATE $? "Start MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections"
 
 systemctl restart mongod
-VALIDATE $? "Restarting Mongodb"
+VALIDATE $? "Restarted MongoDB"
